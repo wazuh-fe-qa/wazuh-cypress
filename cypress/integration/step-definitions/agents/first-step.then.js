8f8d6ca9eea39c645ff2b240749b3719ee07b21d
mauceballos <76791841+mauceballos@users.noreply.github.com>
2022-05-23 09:58:09 -0300

    Update first-step.then.js

--- cypress/integration/step-definitions/agents/first-step.then.js
+++ cypress/integration/step-definitions/agents/first-step.then.js
@@ -1,12 +1,14 @@
-import { And } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { elementIsVisible, xpathElementIsVisible, checkInformationElement } from '../../utils/driver';
 import { operationSystemTitle, operationSystemOption,closeButton } from '../../pageobjects/agents/deploy-new-agent.page';
-And('A first step {string} is displayed and the following {string} options', (title, option) => {
+
+Then('A first step {string} is displayed and the following {string} options', (title, option) => {
     elementIsVisible(operationSystemTitle);
     elementIsVisible(operationSystemOption);
     checkInformationElement(operationSystemTitle, title, 1);
     checkInformationElement(operationSystemOption, option, 4);
 });
-And('An X button in the top right is displayed', () => {
+
+Then('An X button in the top right is displayed', () => {
     xpathElementIsVisible(closeButton);
 });
