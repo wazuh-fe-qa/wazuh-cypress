0d2ea737f2ea52193bee72fe9154e1153baa993d
mauceballos <76791841+mauceballos@users.noreply.github.com>
2022-05-23 10:00:50 -0300

    Update steps-config-agent-page.and.js

--- cypress/integration/step-definitions/agents/steps-config-agent-page.and.js
+++ cypress/integration/step-definitions/agents/steps-config-agent-page.and.js
@@ -1,7 +1,7 @@
-import { And } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { getElement } from '../../utils/driver';
 import { deployNewAgentSections } from '../../pageobjects/agents/deploy-new-agent.page';
 
-And('A box with four steps to the different settings is displayed', () => {
+Then('A box with four steps to the different settings is displayed', () => {
     getElement(deployNewAgentSections).its('length').should('eq', 4)
 });
