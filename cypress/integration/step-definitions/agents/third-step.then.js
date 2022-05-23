08ff5e441e05ae51c5ae4e547457e93071e4aaed
mauceballos <76791841+mauceballos@users.noreply.github.com>
2022-05-23 10:01:25 -0300

    Update third-step.then.js

--- cypress/integration/step-definitions/agents/third-step.then.js
+++ cypress/integration/step-definitions/agents/third-step.then.js
@@ -1,8 +1,8 @@
-import { And } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { elementIsVisible,xpathElementIsVisible, xpathCheckInformationElement, checkInformationElement } from '../../utils/driver';
 import { agentToGroupSubTitle, agentToGroupMessage, agentToGroupSelector } from '../../pageobjects/agents/deploy-new-agent.page';
 
-And('A third step {string} with the {string} are displayed and the following drop-down with Select group by default {string}', (title, discription, information) => {
+Then('A third step {string} with the {string} are displayed and the following drop-down with Select group by default {string}', (title, discription, information) => {
     elementIsVisible(agentToGroupSubTitle);
     xpathElementIsVisible(agentToGroupMessage);
     elementIsVisible(agentToGroupSelector);
