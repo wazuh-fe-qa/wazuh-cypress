3807f68bf5351c409b1be5a53e864d9c1470ce8e
mauceballos <76791841+mauceballos@users.noreply.github.com>
2022-06-08 10:56:12 -0300

    Update validate-wazuh-logo.given.js

--- cypress/integration/step-definitions/settings/validate-wazuh-logo.given.js
+++ cypress/integration/step-definitions/settings/validate-wazuh-logo.given.js
@@ -6,8 +6,6 @@ import { wazuhMenuButton } from '../../pageobjects/wazuh-menu/wazuh-menu.page';
 Given('The kibana admin user is logged in using {} and the wazuh logo is displayed',  (loginMethod) => {
     const url = Cypress.env(loginMethod);
     
-    cy.log(`Parameter url from loginMethod is: ${url}`);
-    
     navigate(Cypress.env(loginMethod));
     
     elementIsVisible(wazuhMenuButton);
@@ -16,8 +14,6 @@ Given('The kibana admin user is logged in using {} and the wazuh logo is display
 
 Given('The admin user is logged in using {}',  (loginMethod) => {
     const url = Cypress.env(loginMethod);
-    
-    cy.log(`Parameter url from loginMethod is: ${url}`);
     
     navigate(Cypress.env(loginMethod));
     
