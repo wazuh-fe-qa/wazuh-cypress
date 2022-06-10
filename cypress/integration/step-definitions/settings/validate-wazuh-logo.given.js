90aa574a46b3f425bd98776c1de76803a523b383
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2022-06-10 15:25:18 -0300

    test

--- cypress/integration/step-definitions/settings/validate-wazuh-logo.given.js
+++ cypress/integration/step-definitions/settings/validate-wazuh-logo.given.js
@@ -1,22 +1,13 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps';
 import { navigate, elementIsVisible } from '../../utils/driver';
 import { wazuhMenuButton } from '../../pageobjects/wazuh-menu/wazuh-menu.page';
-const url = Cypress.env(loginMethod);
 
-Given('The kibana admin user is logged in using {} and the wazuh logo is displayed',  (loginMethod) => {
-    // const url = Cypress.env(loginMethod);
-    
+Given('The kibana admin user is logged in using {} and the wazuh logo is displayed',  () => {
     navigate("app/wazuh");
-    
     elementIsVisible(wazuhMenuButton);
-    
 })
 
-Given('The admin user is logged in using {}',  (loginMethod) => {
-    // const url = Cypress.env(loginMethod);
-    
+Given('The admin user is logged in using {}',  () => {
     navigate("app/wazuh");
-    
     elementIsVisible(wazuhMenuButton);
-    
 })
