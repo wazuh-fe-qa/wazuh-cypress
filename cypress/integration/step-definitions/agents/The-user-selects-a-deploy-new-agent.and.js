a3a2ce0a9cee038c58eb27dc6e628eb0858f80a5
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2022-06-14 16:12:50 -0300

    fix test

--- cypress/integration/step-definitions/agents/The-user-selects-a-deploy-new-agent.and.js
+++ cypress/integration/step-definitions/agents/The-user-selects-a-deploy-new-agent.and.js
@@ -2,7 +2,7 @@ import { When } from 'cypress-cucumber-preprocessor/steps';
 import { clickElementByXpath, xpathElementIsVisible, getSelector } from '../../utils/driver';
 
 import { AGENTS_PAGE as pageName} from '../../utils/pages-constants';
-const deployNewAgentButton = getSelector('deployNewAgentSections', pageName);
+const deployNewAgentButton = getSelector('deployNewAgentButton', pageName);
 
 When('The user selects a deploy new agent', () => {
     xpathElementIsVisible(deployNewAgentButton);
