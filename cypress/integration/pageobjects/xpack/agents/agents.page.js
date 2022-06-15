8c197aa3c1e5d14ae6a68c35d8570fe03e93c5b4
Matias Ezequiel Moreno <matiasmoreno876@gmail.com>
2022-06-15 13:29:13 -0300

    prettier

--- cypress/integration/pageobjects/xpack/agents/agents.page.js
+++ cypress/integration/pageobjects/xpack/agents/agents.page.js
@@ -6,16 +6,14 @@ export const AGENTS_PAGE = {
   agSystemAuditingLink: '.WzManagementSideMenu [title="System Auditing"] div div',
   agVulnerabilitiesLink: '.WzManagementSideMenu [title="Vulnerabilities"] div div',
   agMitreAttackLink: '.WzManagementSideMenu [title="MITRE ATT&CK"] div div',
-  moreLink:
-    '//*[contains(@class,"wz-module-header-agent-main")]//*/button//*/span[contains(text(),"More")]',
+  moreLink: '//*[contains(@class,"wz-module-header-agent-main")]//*/button//*/span[contains(text(),"More")]',
   agPolicyMonitoring: '.WzManagementSideMenu [title="Policy Monitoring"] div div',
   agPCIDSS: '.WzManagementSideMenu [title="PCI DSS"] div div',
   agGDPR: '.WzManagementSideMenu [title="GDPR"] div div',
   agHIPAA: '.WzManagementSideMenu [title="HIPAA"] div div',
   agNIST: '.WzManagementSideMenu [title="NIST 800-53"] div div',
   agTSC: '.WzManagementSideMenu [title="TSC"] div div',
-  statusChart:
-    '.agents-status-pie > .euiPanel > .euiCard__content > .euiCard__children > .euiFlexGroup',
+  statusChart: '.agents-status-pie > .euiPanel > .euiCard__content > .euiCard__children > .euiFlexGroup',
   agentDetails: 'euiFlexItem euiFlexItem--flexGrowZero agents-details-card',
   deployNewAgentButton: '//span[contains(text(),"Deploy new agent")]',
 };
--- cypress/integration/pageobjects/xpack/agents/deploy-new-agent.page.js
+++ cypress/integration/pageobjects/xpack/agents/deploy-new-agent.page.js
@@ -12,34 +12,24 @@ export const DEPLOY_NEW_AGENT_PAGE = {
   operationSystemTitle: "[name='RegisterAgent'] .euiStep:nth-child(1) .euiTitle",
   operationSystemOption: "[name='RegisterAgent'] .euiStep:nth-child(1) fieldset label",
 
-  serverAddressSubTitle:
-    "[name='RegisterAgent'] .euiStep:nth-child(2) :nth-child(1) p.euiStep__title",
+  serverAddressSubTitle: "[name='RegisterAgent'] .euiStep:nth-child(2) :nth-child(1) p.euiStep__title",
   serverMessage: "[name='RegisterAgent'] .euiStep:nth-child(2) .euiStep__content p",
   serverInputField: "[name='RegisterAgent'] .euiStep:nth-child(2) [placeholder='Server address']",
 
   agentToGroupSubTitle: "[name='RegisterAgent'] .euiStep:nth-child(3) .euiTitle",
-  agentToGroupMessage:
-    "//*[@name='RegisterAgent']//*[3][contains(@class,'euiStep')]//*[contains(@class,'euiText')]/p", //cambiar a xpath
-  agentToGroupSelector:
-    "[name='RegisterAgent'] .euiStep:nth-child(3) [data-test-subj='comboBoxInput']",
+  agentToGroupMessage: "//*[@name='RegisterAgent']//*[3][contains(@class,'euiStep')]//*[contains(@class,'euiText')]/p", //cambiar a xpath
+  agentToGroupSelector: "[name='RegisterAgent'] .euiStep:nth-child(3) [data-test-subj='comboBoxInput']",
 
   installAndEnrollAgentSubTitle: "[name='RegisterAgent'] .euiStep:nth-child(4) .euiTitle",
-  installAndEnrollAgentDefaultLabel:
-    "[name='RegisterAgent'] .euiStep:nth-child(4) .euiCallOutHeader__title",
+  installAndEnrollAgentDefaultLabel: "[name='RegisterAgent'] .euiStep:nth-child(4) .euiCallOutHeader__title",
   // after selected these OS options
   installAndEnrollAgentCommand: "[name='RegisterAgent'] .euiStep:nth-child(5) .euiCodeBlock__line",
-  installAndEnrollAgentCopyButton:
-    "[name='RegisterAgent'] .euiStep:nth-child(5) .copy-codeblock-wrapper .copy-overlay",
+  installAndEnrollAgentCopyButton: "[name='RegisterAgent'] .euiStep:nth-child(5) .copy-codeblock-wrapper .copy-overlay",
 
   startTheAgentTitle: "[name='RegisterAgent'] .euiStep:nth-child(6) p",
-  startTheAgentTabsLabels:
-    "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button",
-  systemmdTabButton:
-    "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button:nth-child(1)",
-  sysVTabButton:
-    "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button:nth-child(2)",
-  startTheAgentCommandText:
-    "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content [role='tabpanel'] .euiText pre",
-  startTheAgentcopyButton:
-    "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content [role='tabpanel'] .euiText .copy-overlay",
+  startTheAgentTabsLabels: "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button",
+  systemmdTabButton: "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button:nth-child(1)",
+  sysVTabButton: "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content .euiTabs button:nth-child(2)",
+  startTheAgentCommandText: "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content [role='tabpanel'] .euiText pre",
+  startTheAgentcopyButton: "[name='RegisterAgent'] .euiStep:nth-child(6) .euiStep__content [role='tabpanel'] .euiText .copy-overlay",
 };
