8113b20c78c67fbe22c8791dd44c7668b023ec35
Mayons95 <104914131+Mayons95@users.noreply.github.com>
2022-06-28 13:13:06 -0300

    Update the-user-edit-the-selected-rule-when.js

--- cypress/integration/step-definitions/management/rules/the-user-edit-the-selected-rule-when.js
+++ cypress/integration/step-definitions/management/rules/the-user-edit-the-selected-rule-when.js
@@ -1,6 +1,5 @@
 import { When } from 'cypress-cucumber-preprocessor/steps';
 import { clickElement, elementIsVisible, getSelector, fillField } from '../../../utils/driver';
-
 import { RULES_PAGE as pageName} from '../../../utils/pages-constants';
 const codeEditorSelector = getSelector('codeEditorSelector', pageName);
 
