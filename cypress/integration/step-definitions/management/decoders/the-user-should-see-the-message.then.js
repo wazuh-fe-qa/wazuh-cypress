2c33be0ca455e9bb7d6a19000b1c5d9a3bc9ba3f
Facundo Mayon <mayonf95@gmail.com>
2022-06-28 13:32:21 -0300

    Added edit decoders without save test

--- cypress/integration/step-definitions/management/decoders/the-user-should-see-the-message.then.js
+++ cypress/integration/step-definitions/management/decoders/the-user-should-see-the-message.then.js
@@ -1,6 +1,5 @@
 import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { getElement, getSelector } from '../../../utils/driver';
-
 import { DECODERS_PAGE as pageName} from '../../../utils/pages-constants';
 const buttonRestartSelector = getSelector('buttonRestartSelector', pageName);
 const messageConfirmSaveSelector = getSelector('messageConfirmSaveSelector', pageName);
