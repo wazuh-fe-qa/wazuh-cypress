69354364829f9cf4e15d81fa69a54b3a159a3b98
Mayons95 <104914131+Mayons95@users.noreply.github.com>
2022-06-28 13:27:31 -0300

    Update the-confirm-modal-is-displayed-then.js

--- cypress/integration/step-definitions/management/rules/the-confirm-modal-is-displayed-then.js
+++ cypress/integration/step-definitions/management/rules/the-confirm-modal-is-displayed-then.js
@@ -1,9 +1,9 @@
-import { When } from 'cypress-cucumber-preprocessor/steps';
+import { Then } from 'cypress-cucumber-preprocessor/steps';
 import { clickElement, elementIsVisible, getSelector } from '../../../utils/driver';
 import { RULES_PAGE as pageName} from '../../../utils/pages-constants';
 const confirmModalSelector = getSelector('confirmModalSelector', pageName);
 
-When('The informative modal is displayed', () => {
+Then('The informative modal is displayed', () => {
   elementIsVisible(confirmModalSelector);
   clickElement(confirmModalSelector);
 });
