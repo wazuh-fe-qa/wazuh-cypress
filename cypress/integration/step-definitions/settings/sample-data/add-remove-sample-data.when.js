25216d8421966e1d0f7fbe656c19df72888018a2
mauceballos <mauricioaceballos@gmail.com>
2022-06-29 16:25:52 -0300

    fix for add sample data tc

--- cypress/integration/step-definitions/settings/sample-data/add-remove-sample-data.when.js
+++ cypress/integration/step-definitions/settings/sample-data/add-remove-sample-data.when.js
@@ -1,8 +1,26 @@
 import { clickElement, getSelector } from '../../../utils/driver';
 import { SAMPLE_DATA } from '../../../utils/pages-constants';
-When('The user adds/removes sample data for', (types) => {
+
+
+
+When('The user {} sample data for', (status,types) => {
+  cy.get('[data-test-subj="globalToastList"] div button', { timeout: 8000 });
+  clickElement('[data-test-subj="globalToastList"] div button');
   types.raw().forEach((sample) => {
+    cy.wait(500)
     clickElement(getSelector(sample, SAMPLE_DATA));
-    cy.wait(8000);
+    debugger
+    cy.wait(2000);
+    cy.get('[data-test-subj="globalToastList"] div span', { timeout: 8000 })
+      .then(($) => {
+        const texts = $.map((i, el) => Cypress.$(el).text().replace('A new notification appears').replace('Date range for sample data is now-7 days ago'))
+        const paragraphs = texts.get()
+        let element = getSelector(sample + ' title', SAMPLE_DATA)
+        let titles = Cypress.$(element).text()
+        let titleStatus = 'added'
+        if(status != 'adds') titleStatus =  'removed';
+        expect(paragraphs.toString()).to.equals(titles + ' alerts '+ titleStatus)
+        clickElement('[data-test-subj="globalToastList"] div button');
+      })
   });
 });
