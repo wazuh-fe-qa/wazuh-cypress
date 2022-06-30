def02021f104e6f05ee1402b6bdc3f7bea449da7
mauceballos <mauricioaceballos@gmail.com>
2022-06-30 11:49:46 -0300

    fix for comment

--- cypress/integration/step-definitions/settings/The-wazuh-admin-user-is-logged.given.js
+++ cypress/integration/step-definitions/settings/The-wazuh-admin-user-is-logged.given.js
@@ -1,6 +1,5 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps';
 import { navigate, elementIsVisible, getSelector } from '../../utils/driver';
-
 import { WAZUH_MENU_PAGE as pageName} from '../../utils/pages-constants';
 const wazuhMenuButton = getSelector('wazuhMenuButton', pageName);
 
