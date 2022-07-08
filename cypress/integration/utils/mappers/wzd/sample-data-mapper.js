d0d01d613f28e073b33a88f418ced5f2dda7e831
mauceballos <mauricioaceballos@gmail.com>
2022-07-08 12:56:26 -0300

    fix for sample data test case

--- cypress/integration/utils/mappers/wzd/sample-data-mapper.js
+++ cypress/integration/utils/mappers/wzd/sample-data-mapper.js
@@ -4,6 +4,9 @@ import {
 
 export const SAMPLE_DATA = {
   'security information': SAMPLE_DATA_PAGE['addSecurityInformationDataButton'],
+  'security information title': SAMPLE_DATA_PAGE['securityInformationDataTitle'],
   'auditing and policy monitoring': SAMPLE_DATA_PAGE['addAuditingAndPolicyMonitoringDataButton'],
+  'auditing and policy monitoring title': SAMPLE_DATA_PAGE['auditingAndPolicyMonitoringDataTitle'],
   'threat detection and response': SAMPLE_DATA_PAGE['addThreatDetectionAndResponseDataButton'],
+  'threat detection and response title': SAMPLE_DATA_PAGE['threatDetectionAndResponseDataTitle'],
 };
